55f04b9388bb44670f223c356bc966d5fc629a53
Nilso97 <nilsojunior90@gmail.com>
2023-11-05 19:27:18 -0300

    adicionando lib de log ao projeto

--- core/http/RequisitionService.py
+++ core/http/RequisitionService.py
@@ -1,28 +1,47 @@
 import http.client
 from core.http.IRequisitionService import IRequisitionService
+from core.logger.Logger import Logger
 
 
 class RequisitionService(IRequisitionService):
 
     def __init__(self) -> None:
         self.conn = http.client.HTTPSConnection(host='kabum.com.br', port=443)
+        self.logger = Logger()
 
-    def send_http_client(self, method: str, url: str):
+    def send_http_client(self, method: str, url: str, body: dict = {}):
+        """"""
         try:
-            self.conn.request(
-                method=method,
-                url=url,
-                headers={
-                    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36'
-                }
-            )
-            response = self.convert_bytes_in_str(
-                response=self.conn.getresponse())
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36' +
+                '(KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36'
+            }
+            if method == 'get':
+                self.conn.request(
+                    method='GET',
+                    url=url,
+                    headers=headers
+                )
+            elif method == 'post':
+                self.conn.request(
+                    method='POST',
+                    url=url,
+                    headers=headers,
+                    body=body
+                )
+            response = self.conn.getresponse()
+
+            if response.status != 200 or not response:
+                raise Exception('O site "kabum.com.br" está fora do ar!')
+
+            final_response = self.convert_bytes_in_str(response=response)
         except (Exception) as error:
-            response = error
-            print('Erro na função "send_http_client()" ->', str(error))
+            final_response = error
+            self.logger.send_error_message(
+                'Erro na função "send_http_client()" ->', str(error))
         finally:
-            return response
+            return final_response
 
     def convert_bytes_in_str(self, response: bytes) -> str:
+        """"""
         return response.read().decode('utf-8')
--- src/KabumScrapService.py
+++ src/KabumScrapService.py
@@ -1,7 +1,7 @@
 import json
-import logging
 import time
 from core.http.RequisitionService import RequisitionService
+from core.logger.Logger import Logger
 
 
 class KabumScrapService():
@@ -10,57 +10,67 @@ class KabumScrapService():
         self.products_list = []
         self.search_price = search_price
         self.scrap_core = RequisitionService()
+        self.logger = Logger()
 
-        self.min_value = 1000 # Setar valor mínimo da busca
+        self.min_value = 1000  # Setar valor mínimo da busca
 
     def scrap_init(self) -> None:
-        scrap_result = None
+        """"""
         try:
             site_response = self.scrap_core.send_http_client(
-                method='GET', url='https://www.kabum.com.br/')
+                method='get', url='https://www.kabum.com.br/', body=None)
             if site_response is None:
                 raise Exception('O site "kabum.com.br" está fora do ar!')
 
-            print(f'Iniciando a busca por produtos no site "kabum.com.br" com valor mínimo de '+
-                   f'R${self.min_value} até R${self.search_price}')
-            search_url = self.get_search_url(page_number=1)
+            self.logger.send_info_message(f'Iniciando a busca por produtos no site "kabum.com.br" com valor mínimo de ' +
+                                          f'R${self.min_value} até R${self.search_price}')
+
+            search_url = self.get_default_endpoint(page_number=1)
             time.sleep(5)
             response = self.scrap_core.send_http_client(
-                method='GET',
-                url=search_url
+                method='get',
+                url=search_url,
+                body=None
             )
-            page_number = 2
-            total_pages = json.loads(response).get('meta')['total_pages_count']
             self.get_products_data(products_data=json.loads(response))
-            while total_pages >= page_number:
-                print(f'Realizando a pesquisa na página {page_number} de {total_pages}')
-                # time.sleep(1)
-                paginate_response = self.get_consult_pagination(
-                    page_number=page_number)
-                self.get_products_data(
-                    products_data=json.loads(paginate_response))
-                page_number += 1
-            self.make_product_list()
-            print('Foi finalizada com sucesso a busca por produtos no site "kabum.com.br"!')
-            print(f'Foram encontrados {len(self.products_list)} produtos, dentre os valores inseridos na pesquisa')
+            total_pages = json.loads(response).get('meta')['total_pages_count']
+            if total_pages > 1:
+                self.get_search_pagination(
+                    page_number=2, total_pages=total_pages)
+
+            self.make_product_json()
+
+            self.logger.send_info_message(
+                'Foi finalizada com sucesso a busca por produtos no site "kabum.com.br"!')
+            self.logger.send_info_message(
+                f'Foram encontrados {len(self.products_list)} produtos, dentre os valores inseridos na pesquisa')
         except (Exception) as error:
             scrap_result = error
-            print('Erro na função "scrap_request()" ->', str(error))
+            self.logger.send_error_message(
+                'Erro na função "scrap_request()" ->', str(error))
         finally:
             return scrap_result
 
-    def make_product_list(self) -> None:
-        print('Gerando arquivo ".json" contendo os produtos encontrados')
-        with open("./produtos.json", "w") as file:
-            json.dump(self.products_list, file, indent=4)
+    def make_product_json(self) -> None:
+        """"""
+        try:
+            self.logger.send_info_message(
+                'Gerando arquivo ".json" contendo os produtos encontrados')
+            with open("./produtos.json", "w") as file:
+                json.dump(self.products_list, file, indent=4)
+        except (Exception) as error:
+            self.logger.send_error_message(
+                'Erro na função "make_product_json()" ->', str(error))
 
     def get_products_data(self, products_data: dict) -> dict:
+        """"""
         for product in products_data.get('data'):
             if product.get("attributes")["price"] <= self.search_price\
                     and product.get("attributes")["price"] > self.min_value:
                 product_price = f'R${product.get("attributes")["price"]}'
-                print(f'Foi encontrado um produto no valor de {product_price}, ' +
-                      f'na página {products_data.get("meta")["page"]["number"]}')
+                self.logger.send_info_message(f'Foi encontrado um produto no valor de {product_price}, ' +
+                                              f'na página {products_data.get("meta")["page"]["number"]}')
+                time.sleep(1)
                 self.products_list.append({
                     'Id': product.get('id'),
                     'Produto': product.get('attributes')['title'],
@@ -72,6 +82,32 @@ class KabumScrapService():
                 })
         return
 
+    def get_search_pagination(self, page_number: int, total_pages: int):
+        """"""
+        try:
+            while total_pages >= page_number:
+                self.logger.send_info_message(
+                    f'Realizando a pesquisa na página {page_number} de {total_pages}')
+                time.sleep(1)
+                paginate_response = self.scrap_core.send_http_client(
+                    method='get', url=self.get_default_endpoint(page_number=page_number), body=None)
+                self.get_products_data(
+                    products_data=json.loads(paginate_response))
+                page_number += 1
+        except (Exception) as error:
+            paginate_response = error
+            self.logger.send_error_message(
+                'Erro na função "get_search_pagination()"', str(error))
+        finally:
+            return paginate_response
+
+    def get_default_endpoint(self, page_number: int) -> str:
+        """"""
+        default_endpoint = 'https://servicespub.prod.api.aws.grupokabum.com.br'
+        default_endpoint += '/catalog/v2/products-by-category/computadores/notebooks?'
+        default_endpoint += f'page_number={page_number}&page_size=20&facet_filters=&sort=most_searched&include=gift'
+        return default_endpoint
+
     def get_value_with_discount_prime_ninja(self, product: dict) -> str:
         return f'R${product.get("attributes")["prime"]["price_with_discount"]}' if product.get("attributes").get("prime") else ''
 
@@ -80,14 +116,3 @@ class KabumScrapService():
 
     def get_value_black_friday_with_discount(self, product: dict) -> str:
         return f'R${product.get("attributes")["offer"]["price_with_discount"]}' if product.get("attributes").get("offer") else ''
-
-    def get_consult_pagination(self, page_number: int):
-        paginate_response = self.scrap_core.send_http_client(
-            method='GET', url=self.get_search_url(page_number=page_number))
-        return paginate_response
-
-    def get_search_url(self, page_number: int) -> str:
-        paginate_url = 'https://servicespub.prod.api.aws.grupokabum.com.br'
-        paginate_url += '/catalog/v2/products-by-category/computadores/notebooks?'
-        paginate_url += f'page_number={page_number}&page_size=20&facet_filters=&sort=most_searched&include=gift'
-        return paginate_url
