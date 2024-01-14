cc3726193a4699eb1b76073fc831703bc819929e
Nilso Junior <nilso.junior@docato.com.br>
2024-01-14 20:47:11 -0300

    removendo importações

--- src/database/DatabaseContext.py
+++ src/database/DatabaseContext.py
@@ -1,7 +1,5 @@
-from sqlalchemy import Integer, String
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import Mapped, mapped_column
 
 
 class DatabaseContext(DeclarativeBase):
