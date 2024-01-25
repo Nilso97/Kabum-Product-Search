3abb9454891e6f46acb076df12f8e85bb16b87c7
Nilso Junior <96146165+Nilso97@users.noreply.github.com>
2024-01-24 21:27:04 -0300

    Atualizar o IRequisitionService.py

--- src/core/http/IRequisitionService.py
+++ src/core/http/IRequisitionService.py
@@ -8,10 +8,10 @@ class IRequisitionService(ABC):
     def send_http_client(self, method: str, url: str, body: dict = {}) -> str:
         raise NotImplementedError("Método 'send_http_client' não foi implementado corretamente")
 
-    @abstractmethod
-    def convert_request_params(self, method: str) -> str:
+    @staticmethod
+    def convert_request_params(method: str) -> str:
         raise NotImplementedError("Método 'convert_request_params' não foi implementado corretamente")
 
-    @abstractmethod
-    def convert_response_data(self, response: http.client.HTTPResponse) -> str:
+    @staticmethod
+    def convert_response_data(response: http.client.HTTPResponse) -> str:
         raise NotImplementedError("Método 'convert_response_data' não foi implementado corretamente")
